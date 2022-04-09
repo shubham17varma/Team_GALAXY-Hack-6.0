77fc2574d10e062071a5104355c73309693f2d39
Amaan Shaikh <79324512+Amaan-8808@users.noreply.github.com>
2022-04-09 20:21:54 +0530

    Update hellotest.py

--- hellotest.py
+++ hellotest.py
@@ -1,13 +1,26 @@
-import requests
+from asyncio import sleep
+import os
+import selenium
+from selenium import webdriver
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import ElementClickInterceptedException
 from bs4 import BeautifulSoup
-import lxml
- 
- 
-url = 'https://450dsa.com/'
-reqs = requests.get(url).text
-soup = BeautifulSoup(reqs, 'lxml')
+from selenium.webdriver.common.by import By
 
-data = soup.findAll('link')
+url = "https://450dsa.com/"
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get(url)
+# Wait for the page to fully render before parsing it
 
-    
-print(data)
+time.sleep(10)
+
+# traverse list
+elems = driver.find_elements_by_xpath("//a[@href]")
+for elem in elems:
+    print(elem.get_attribute("href"))
+
+
+
+
+driver.quit()
