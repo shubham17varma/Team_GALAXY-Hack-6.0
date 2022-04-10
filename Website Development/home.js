a321edbfc4d8447c85d04be8795ea3508626d2a8
mayankmk03 <85782107+mayankmk03@users.noreply.github.com>
2022-04-10 07:43:31 +0530

    Javascript

--- Website Development/home.js
+++ Website Development/home.js
@@ -1,5 +1,13 @@
 const express = require("express");
-
 const app = express();
 
-app.listen(3000);
+app.listen(3000, function(){
+  console.log("Server running on port 3000!");
+});
+
+app.use(express.static("public"))
+app.set('view engine', 'ejs');
+
+app.get("/", function (req, res) {
+  res.render("home.ejs");
+})
