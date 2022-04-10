bac9f1df9a7e02e705579cbf66198d37eb9a54b0
Shubham Anil Varma <shubham17varma@gmail.com>
2022-04-10 08:01:51 +0530

    Revert "Javascript"
    This reverts commit a321edbfc4d8447c85d04be8795ea3508626d2a8.

--- Website Development/home.js
+++ Website Development/home.js
@@ -1,13 +1,5 @@
 const express = require("express");
+
 const app = express();
 
-app.listen(3000, function(){
-  console.log("Server running on port 3000!");
-});
-
-app.use(express.static("public"))
-app.set('view engine', 'ejs');
-
-app.get("/", function (req, res) {
-  res.render("home.ejs");
-})
+app.listen(3000);
